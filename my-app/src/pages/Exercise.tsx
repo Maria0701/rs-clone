0f9d3f88df29c03e3cc6298ba0da2a7f95cecc67
AndreiArsiomenko <andartsiomenko@gmail.com>
2023-02-15 20:02:42 +0300

    feat: add timer for exercise

--- my-app/src/pages/Exercise.tsx
+++ my-app/src/pages/Exercise.tsx
@@ -1,44 +1,75 @@
-import { useState, FC } from 'react';
-import YouTube from 'react-youtube';
+import { useState, FC, useEffect } from 'react';
+import YouTube, { YouTubePlayer } from 'react-youtube';
 import { useParams } from 'react-router';
 import { ITraining, ITrainings } from '../models/models';
 
 export const Exercise: FC<ITrainings> = ({ trainings }) => {
-  const [player, setPlayer] = useState();
+  const [player, setPlayer] = useState<YouTubePlayer>(null);
+  const [duration, setDuration] = useState(0);
+  const [currentTime, setCurrentTime] = useState(0);
+  const [pausedTimer, setPausedTimer] = useState<boolean>(false);
+  const [startTimer, setStartTimer] = useState<boolean>(false);
+
   const { id } = useParams();
   const training = trainings.find((item) => item.id === id) as ITraining;
   const youtubeID = training.url;
 
-  const onReady = (event) => {
+  const onReady = (event: YouTubePlayer) => {
     setPlayer(event.target);
+    setDuration(event.target.getDuration())
+    setCurrentTime(duration)
   };
 
   const onPlay = () => {
     player.playVideo();
+    setPausedTimer(false)
+    setStartTimer(true)
   };
 
   const onPause = () => {
     player.pauseVideo();
+    setPausedTimer(true);
+    setStartTimer(false)
   };
 
   const onStop = () => {
     player.stopVideo();
+    setStartTimer(false);
+    setCurrentTime(duration);
   };
+
+  const tick = () => {
+    if (pausedTimer) return;
+
+    if (currentTime === 0) {
+      setCurrentTime(duration);
+      setStartTimer(false);
+    } else {
+      setCurrentTime(currentTime - 1);
+    }
+  };
+
+  useEffect(() => {
+    if (startTimer) {
+      const timerID = setInterval(() => tick(), 1000);
+      return () => clearInterval(timerID);
+    }
+  }, [currentTime, startTimer]);
 
   const options = {
     height: "390",
     width: "640",
     playerVars: {
-      'autoplay': 0,
-      'controls': 0,
-      'fs': 0,
-      'modestbranding': 1,
-      'disablekb': 1,
-      'showinfo': 0,
-      'rel': 0,
-      'iv_load_policy': 3,
-      'origin': 0,
-      'enablejsapi': 1,
+      autoplay: 0,
+      controls: 0,
+      fs: 0,
+      modestbranding: 1,
+      disablekb: 1,
+      showinfo: 0,
+      rel: 0,
+      iv_load_policy: 3,
+      origin: 'https://localhost:3000',
+      enablejsapi: 1,
     },
   };
 
@@ -54,7 +85,7 @@ export const Exercise: FC<ITrainings> = ({ trainings }) => {
       </div>
       <div className="exercise__btns">
         <button onClick={onPlay} className="btn">
-          Play
+          Start
         </button>
         <button onClick={onPause} className="btn">
           Pause
@@ -62,6 +93,7 @@ export const Exercise: FC<ITrainings> = ({ trainings }) => {
         <button onClick={onStop} className="btn">
           Stop
         </button>
+        <div>Timer: {currentTime}</div>
       </div>
     </div>
   );
